1a275df98274d05c26c5876a5a237345bf45dddd
MurtazaAritha <murtaza.salumber@arithaconsulting.com>
2024-12-31 11:50:39 +0530

    changes in config file

--- vite.config.mjs
+++ vite.config.mjs
@@ -28,6 +28,7 @@ export default defineConfig({
   server: {
     // this ensures that the browser opens upon server start
     open: true,
+    host:'0.0.0.0',
     // this sets a default port to 3000
     port: 3000
   },
