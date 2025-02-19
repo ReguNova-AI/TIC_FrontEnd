a69d977258031c65ec9f8302a9ffa0de4a26331b
MurtazaAritha <murtaza.salumber@arithaconsulting.com>
2025-02-19 12:11:15 +0530

    fixed search in super admin

--- src/pages/ProjectListing/AdminOrgNestedListing.jsx
+++ src/pages/ProjectListing/AdminOrgNestedListing.jsx
@@ -1,59 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { Table, Avatar, Input,Space } from 'antd';
-import { ApartmentOutlined } from '@ant-design/icons';
+import { Table, Avatar, Input, Space } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import { Chip } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import { formatDate, getStatusChipProps } from "shared/utility";
-import { BUTTON_LABEL, LISTING_PAGE ,FORM_LABEL } from 'shared/constants';
-import { useLocation, useNavigate } from "react-router-dom";
+import { BUTTON_LABEL, LISTING_PAGE, FORM_LABEL } from 'shared/constants';
+import { useNavigate } from "react-router-dom";
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
-import {SearchOutlined} from "@ant-design/icons";
-import Orgicon from "../../assets/images/icons/orgListing.svg"
-import projectIcon from "../../assets/images/icons/projectIcon3.svg"
-
+import Orgicon from "../../assets/images/icons/orgListing.svg";
+import projectIcon from "../../assets/images/icons/projectIcon3.svg";
 
 // AdminOrgNestedListing Component
-const AdminOrgNestedListing = ({data}) => {
+const AdminOrgNestedListing = ({ data }) => {
   const navigate = useNavigate();
   const [dataSource, setDataSource] = useState([]);
   const [searchText, setSearchText] = useState('');
 
   // Fetch data when the component mounts
   useEffect(() => {
-    const fetchData = async () => {
-        setDataSource(data)
-    };
-    fetchData();
+    setDataSource(data);
   }, [data]);
 
+  // Function to navigate to project view
   const handleNavigateToProject = (projectNo) => {
     navigate(`/projectView/${projectNo}`, { state: { projectNo } });
   };
 
-  useEffect(()=>{
-    filterData(data)
-  },[searchText]);
+  // Update filtered data whenever searchText changes
+  useEffect(() => {
+    const filteredData = filterData(data);
+    setDataSource(filteredData);
+  }, [searchText, data]);
 
-
+  // Columns for Project table
   const projectColumns = [
     {
       title: 'Project Name',
       dataIndex: 'project_name',
       key: 'project_name',
-      render: (text, record) => {return(
-        <>
-        <img src={projectIcon} width="30px" style={{verticalAlign:"middle",marginRight:"10px"}}/>
-        <a
-          onClick={() => handleNavigateToProject(record.project_id)}
-          style={{ color: "#2ba9bc", cursor: "pointer" }}
-        >
-          {record.project_name}
-        </a>
-        </>
-      )},
+      render: (text, record) => {
+        return (
+          <>
+            <img src={projectIcon} width="30px" style={{ verticalAlign: "middle", marginRight: "10px" }} />
+            <a
+              onClick={() => handleNavigateToProject(record.project_id)}
+              style={{ color: "#2ba9bc", cursor: "pointer" }}
+            >
+              {record.project_name}
+            </a>
+          </>
+        );
+      },
     },
     {
       title: 'Project No',
@@ -74,13 +74,13 @@ const AdminOrgNestedListing = ({data}) => {
       title: 'Start Date',
       dataIndex: 'created_at',
       key: 'created_at',
-      render: (created_at) => created_at && created_at != "null" ? formatDate(created_at) : "",
+      render: (created_at) => created_at && created_at !== "null" ? formatDate(created_at) : "",
     },
     {
       title: 'Last Run',
       dataIndex: 'last_run',
       key: 'last_run',
-      render: (last_run) => last_run && last_run != "null" ? formatDate(last_run) : "",
+      render: (last_run) => last_run && last_run !== "null" ? formatDate(last_run) : "",
     },
     {
       title: 'Status',
@@ -113,7 +113,7 @@ const AdminOrgNestedListing = ({data}) => {
     },
   ];
 
-  // Expandable columns for Industry details
+  // Columns for Industry table
   const industryColumns = [
     {
       title: 'Industry Name',
@@ -128,7 +128,7 @@ const AdminOrgNestedListing = ({data}) => {
     },
   ];
 
-  // Columns for Organization listing
+  // Columns for Organization table
   const columns = [
     {
       title: 'Organization',
@@ -136,47 +136,35 @@ const AdminOrgNestedListing = ({data}) => {
       key: 'org_name',
       render: (value, record) => (
         <>
-        {record.org_logo ?
-          <Avatar
-            sx={{ width: 40, height: 40 }}
-            alt={record.org_name}
-            src={record.org_logo}
-          />
-          :
-          <img src={Orgicon} width="32px" style={{verticalAlign:"middle"}}/>
-        }
+          {record.org_logo ? (
+            <Avatar sx={{ width: 40, height: 40 }} alt={record.org_name} src={record.org_logo} />
+          ) : (
+            <img src={Orgicon} width="32px" style={{ verticalAlign: "middle" }} />
+          )}
           <span style={{ marginLeft: 10 }}>{value}</span>
         </>
       ),
     },
-    // {
-    //   title: 'Sector',
-    //   dataIndex: 'sector_name',
-    //   key: 'sector_name',
-    // },
     {
-        title: 'Total Industries',
-        dataIndex: 'industries',
-        key: 'industries',
-        render: (industries) => industries ? industries.length : 0,
+      title: 'Total Industries',
+      dataIndex: 'industries',
+      key: 'industries',
+      render: (industries) => industries ? industries.length : 0,
+    },
+    {
+      title: 'Total Projects',
+      key: 'total_projects',
+      render: (_, record) => {
+        const totalProjects = record.industries.reduce((acc, industry) => {
+          return acc + (industry.projects ? industry.projects.length : 0);
+        }, 0);
+        return <span>{totalProjects}</span>;
       },
-
-      {
-        title: 'Total Projects',
-        key: 'total_projects',
-        render: (_, record) => {
-          // Calculate the total number of projects for the organization
-          const totalProjects = record.industries.reduce((acc, industry) => {
-            return acc + (industry.projects ? industry.projects.length : 0);
-          }, 0);
-          return <span>{totalProjects}</span>;
-        },
-    }
+    },
   ];
 
   // Filter function based on search text
   const filterData = (data) => {
-    
     return data.filter((org) => {
       const orgMatches = org.org_name.toLowerCase().includes(searchText.toLowerCase());
       const industryMatches = org.industries.some(industry =>
@@ -219,29 +207,26 @@ const AdminOrgNestedListing = ({data}) => {
   return (
     <>
       {/* Search input */}
-
-      <Space style={{float:"right", marginBottom:"20px"}}>
-      <FormControl >
-                <InputLabel htmlFor="outlined-adornment-search">
-                Search by organization, industry, or project name
-                </InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-search"
-                  startAdornment={
-                    <InputAdornment position="start">
-                      <SearchOutlined />
-                    </InputAdornment>
-                  }
-                  label={FORM_LABEL.SEARCH}
-                  onChange={handleSearchChange}
-                  style={{width:"400px"}}
-                />
-              </FormControl>
-              </Space>
-
+      <Space style={{ float: "right", marginBottom: "20px" }}>
+        <FormControl>
+          <InputLabel htmlFor="outlined-adornment-search">
+            Search by organization, industry, or project name
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-search"
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchOutlined />
+              </InputAdornment>
+            }
+            label={FORM_LABEL.SEARCH}
+            onChange={handleSearchChange}
+            style={{ width: "400px" }}
+          />
+        </FormControl>
+      </Space>
 
       {/* Table rendering */}
-      {console.log("dataSource",dataSource)}
       <Table
         columns={columns}
         dataSource={dataSource} // Apply the filter
@@ -253,6 +238,5 @@ const AdminOrgNestedListing = ({data}) => {
     </>
   );
 };
-
 
 export default AdminOrgNestedListing;
